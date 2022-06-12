c40e804d171dfabe0f154a7d3bd731f68c4b8047
ajflore7 <107015928+ajflore7@users.noreply.github.com>
2022-06-12 15:44:11 -0500

    Add files via upload

---
+++ Homework_1/main.py
@@ -0,0 +1,25 @@
+# Anthony Flores 1792816
+import math
+
+height = int(input('Enter wall height (feet):' '\n'))
+
+width = int(input('Enter wall width (feet):' '\n'))
+
+wall_area = height * width
+
+print('Wall area:', wall_area, 'square feet')
+
+gallon_paint = 350
+paint_area = wall_area / gallon_paint
+
+print('Paint needed:', '{:.2f}'.format(wall_area / gallon_paint), 'gallons')
+cans = int(math.ceil(paint_area))
+print('Cans needed: {} can(s)'.format(cans))
+print()
+
+ColorDict = {'red': 35, 'blue': 25, 'green': 23}
+color = input('Choose a color to paint the wall:' '\n')
+
+color_cost = ColorDict.get(color)
+actual_cost = cans * color_cost
+print(f'Cost of purchasing {color} paint: ${actual_cost}')
