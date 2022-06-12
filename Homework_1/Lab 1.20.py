f56ccccb7d28864364fa7d35c2695f7ad7539156
ajflore7 <107015928+ajflore7@users.noreply.github.com>
2022-06-12 15:53:31 -0500

    Add files via upload

---
+++ Homework_1/Lab 1.20.py
@@ -0,0 +1,9 @@
+#Anthony Flores 1792816 CIS 2348 1.20
+
+user_num = int(input('Enter integer:\n'))
+print('You entered:', user_num)
+print(user_num, 'squared is', user_num * user_num)
+print('And', user_num, 'cubed is', user_num ** 3,'!!')
+second_user = int(input('Enter another integer:\n'))
+print(user_num, '+',second_user, 'is',user_num + second_user)
+print(user_num, '*',second_user, 'is',user_num * second_user)
