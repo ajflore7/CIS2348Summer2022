b2bd8eef7a54880d9deb635b5adae9450c699cb0
ajflore7 <107015928+ajflore7@users.noreply.github.com>
2022-06-12 15:54:02 -0500

    Delete main.py

--- Homework_1/main.py
+++
@@ -1,9 +0,0 @@
-#Anthony Flores 1792816 CIS 2348 1.20
-
-user_num = int(input('Enter integer:\n'))
-print('You entered:', user_num)
-print(user_num, 'squared is', user_num * user_num)
-print('And', user_num, 'cubed is', user_num ** 3,'!!')
-second_user = int(input('Enter another integer:\n'))
-print(user_num, '+',second_user, 'is',user_num + second_user)
-print(user_num, '*',second_user, 'is',user_num * second_user)
