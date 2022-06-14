4cbe45965c036966ba12e52eae0ad4143d50b531
ajflore7 <107015928+ajflore7@users.noreply.github.com>
2022-06-13 21:50:57 -0500

    Update Lab 2.19.py

--- Homework_1/Lab 2.19.py
+++ Homework_1/Lab 2.19.py
@@ -1,4 +1,4 @@
-#Anthony Flores 1792816 CIS2348 2.19
+# Anthony Flores 1792816 CIS2348 2.19
 
 lemon_juice = int(input('Enter amount of lemon juice (in cups):\n'))
 Water = int(input('Enter amount of water (in cups):\n'))
@@ -9,22 +9,19 @@ print('Lemonade ingredients - yields', '{:.2f}'.format(Servings), 'servings')
 print('{:.2f}'.format(lemon_juice), 'cup(s) lemon juice')
 print('{:.2f}'.format(Water), 'cup(s) water')
 print('{:.2f}'.format(Agave_nectar), 'cup(s) agave nectar')
-print() #end of question (1)
+print()  # end of question (1)
 
-
-sec_lemon_juice = lemon_juice * 8
-sec_water = Water * 8
-sec_Agave_nectar = Agave_nectar * 8
 sec_servings = int(input('How many servings would you like to make?''\n'))
 print()
 
 print('Lemonade ingredients - yields', '{:.2f}'.format(sec_servings), 'servings')
-print('{:.2f}'.format(sec_lemon_juice), 'cup(s) lemon juice')
-print('{:.2f}'.format(sec_water), 'cup(s) water')
-print('{:.2f}'.format(sec_Agave_nectar), 'cup(s) agave nectar')
+print('{:.2f}'.format(lemon_juice * sec_servings / Servings), 'cup(s) lemon juice')
+print('{:.2f}'.format(Water * sec_servings / Servings), 'cup(s) water')
+print('{:.2f}'.format(Agave_nectar * sec_servings / Servings), 'cup(s) agave nectar')
 print()
 
 print('Lemonade ingredients - yields', '{:.2f}'.format(sec_servings), 'servings')
-print('{:.2f}'.format(sec_lemon_juice / 16), 'gallon(s) lemon juice')
-print('{:.2f}'.format(sec_water / 16), 'gallon(s) water')
-print('{:.2f}'.format(sec_Agave_nectar / 16), 'gallon(s) agave nectar')
+print('{:.2f}'.format(lemon_juice * sec_servings / Servings / 16), 'gallon(s) lemon juice')
+print('{:.2f}'.format(Water * sec_servings / Servings / 16), 'gallon(s) water')
+print('{:.2f}'.format(Agave_nectar * sec_servings / Servings / 16), 'gallon(s) agave nectar')
+
