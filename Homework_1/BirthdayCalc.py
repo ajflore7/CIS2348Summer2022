3e106e9a60c5259b00bd98c0d9a313c67836bab7
ajflore7 <107015928+ajflore7@users.noreply.github.com>
2022-06-13 21:34:18 -0500

    Update BirthdayCalc.py

--- Homework_1/BirthdayCalc.py
+++ Homework_1/BirthdayCalc.py
@@ -12,8 +12,6 @@ Birth_day = int(input('Day (DD):'))
 Birth_year = int(input('Year (YYYY):'))
 age = Current_year - Birth_year
 
-if Current_month == Birth_month:
-    if Current_day == Birth_day:
+if Current_month == Birth_month and Current_day == Birth_day:
         print('WOW today is your birthday, Happy Birthday!', age)
-else:
-    print('You are', age, 'years old')
+print('You are', age, 'years old')
